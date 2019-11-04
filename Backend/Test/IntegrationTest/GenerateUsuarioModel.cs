5549f540c3dbc2753592f2abd9fb3172b6afec08
Igor Matheus <igorm1411@gmail.com>
2019-11-04 04:31:26 -0200

    Commit 12 - Testes de integração

---
+++ Backend/Test/IntegrationTest/GenerateUsuarioModel.cs
@@ -0,0 +1,21 @@
+﻿namespace IntegrationTest
+{
+    using Application.DTO.Usuario;
+    using Bogus;
+    using Domain.Model.Enumerators;
+
+    public class GenerateUsuarioModel
+    {
+        public UsuarioDTO usuarioDTOMock
+        {
+            get => new Faker<UsuarioDTO>()
+                .StrictMode(true)
+                .RuleFor(o => o.Nome, f => f.Person.FirstName)
+                .RuleFor(o => o.Sobrenome, f => f.Person.LastName)
+                .RuleFor(o => o.DataNascimento, f => f.Date.Past())
+                .RuleFor(o => o.Email, f => f.Person.Email)
+                .RuleFor(o => o.Escolaridade, f => (EscolaridadeEnum)f.Random.Number(1, 4))
+                .Generate();
+        }
+    }
+}
---
+++ Backend/Test/IntegrationTest/IntegrationTestConfitec.cs
@@ -0,0 +1,93 @@
+namespace Tests
+{
+    using Application.Abstraction.Services;
+    using Application.DTO.Usuario;
+    using ConfitecProject;
+    using IntegrationTest;
+    using Lambda3.AspNetCore.Mvc.Testing;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.DependencyInjection;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using WebAPI.Controllers;
+
+    public class IntegrationTestConfitec
+    {
+        private WebApplicationFactory<Startup> webAppFactory;
+        private IServiceScope scope;
+        protected IServiceProvider serviceProvider;
+        protected HttpClient client;
+        public UsuarioController usuarioController;
+        public GenerateUsuarioModel generateUsuarioModel => new GenerateUsuarioModel();
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            webAppFactory = new WebApplicationFactory<Startup>(port: 4972);
+            webAppFactory.CreateDefaultClient();
+            client = webAppFactory.CreateClient();
+            scope = webAppFactory.Host.Services.CreateScope();
+            serviceProvider = scope.ServiceProvider;
+            usuarioController = new UsuarioController(serviceProvider.GetService<IUsuarioService>());
+        }
+
+        [Test]
+        public async Task IntegrationCRUDTest()
+        {
+            // Create
+            var usuario = generateUsuarioModel.usuarioDTOMock;
+            await CreateAsync(usuario);
+            var usuarioGet = (await GetAllAsync()).Where(x => x.Nome == usuario.Nome && x.Email == usuario.Email).LastOrDefault();
+            Assert.NotNull(usuarioGet);
+
+            // Update
+            usuario.Nome = $"{usuario.Nome} teste Update";
+            await UpdateAsync(usuario);
+            usuarioGet = await GetByIdAsync(usuarioGet.Id);
+            Assert.Equals(usuarioGet.Nome, usuario.Nome);
+
+            // Delete
+            await DeleteAsync(usuarioGet.Id);            
+        }
+        internal async Task<List<UsuarioDTO>> GetAllAsync()
+        {
+            var result = await usuarioController.GetAsync();
+            var usuarioResult = ((OkObjectResult)result).Value;
+            return (List<UsuarioDTO>)usuarioResult;
+        }
+        internal async Task<UsuarioDTO> GetByIdAsync(int id)
+        {
+            var result = await usuarioController.GetPeloIdAsync(id);
+            var usuarioResult = ((OkObjectResult)result).Value;
+            return (UsuarioDTO)usuarioResult;
+        }
+
+        internal async Task CreateAsync(UsuarioDTO usuarioViewModel)
+        {
+            var result = await usuarioController.PostAsync(usuarioViewModel);
+            Assert.True(((OkResult)result).StatusCode.Equals(StatusCodes.Status200OK));
+        }
+
+        internal async Task UpdateAsync(UsuarioDTO usuarioViewModel)
+        {
+            var result = await usuarioController.PutAsync(usuarioViewModel);
+            Assert.True(((OkResult)result).StatusCode.Equals(StatusCodes.Status200OK));
+        }
+
+        internal async Task DeleteAsync(int id)
+        {
+            var result = await usuarioController.DeleteAsync(id);
+            Assert.True(((OkResult)result).StatusCode.Equals(StatusCodes.Status200OK));
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown() {
+            webAppFactory?.Dispose();
+        }
+    }
+}
